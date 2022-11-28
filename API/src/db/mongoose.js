3fa52fefdae8dd6a35f48bf2df9691b177d4cda0
Ravindu Mihiranga <e18224@eng.pdn.ac.lk>
2022-11-28 10:21:38 +0530

    added the required files for the api

---
+++ API/src/db/mongoose.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/compost-management-system-api", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
---
+++ API/src/index.js
@@ -0,0 +1,105 @@
+const express = require("express");
+require("./db/mongoose.js");
+const Bin_Data = require("./models/bindata.js");
+const Bin = require("./models/bins.js");
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Enpoint for adding data of a bin
+app.post("/api/bindata", function(req, res){
+    const binData = new Bin_Data(req.body);
+
+    binData.save().then(function(){
+        res.status(201).send(binData);
+    }).catch(function(error){
+        res.status(400).send(error);
+    });
+});
+
+// Enpoint for reading all the bins data
+app.get("/api/bindata", function(req,res){
+    Bin_Data.find({}).then(function(binsData){
+        res.send(binsData);
+    }).catch(function(error){
+        res.status(500).send(error);
+    })
+})
+
+// Enpoint for reading a data of a bin
+app.get("/api/bindata/:id", function(req,res){
+    Bin_Data.findById(req.params.id).then(function(binsData){
+        if(!binsData){
+            return res.status(404).send({error: "Bin not found"});
+        } 
+        res.send(binsData);
+        
+    }).catch(function(error){
+        res.status(400).send(error);
+    });
+});
+
+// Enpoint for updating a data of a bin
+app.patch("/api/binsdata/:id", function(req,res){
+    const id = req.params.id;
+    const allowedFields = ["bin id", "day", "quarter", "temperature", "humidity", "compost status"];
+    const updateFields = Object.keys(req.body);
+
+    const isValid = updateFields.every(function(field){
+        return allowedFields.includes(field); // no matter how many trues you have if you have one false then it will return false
+    });
+
+    if(!isValid){
+        return res.status(400).send({error: "Invalid Update"});
+    }
+
+    Bin_Data.findByIdAndUpdate(id, req.body, {new: true}).then((binsData) => {
+        if(!binsData){
+            return res.status(404).send({error: "Bin not found"});
+        } 
+        res.send(binsData);
+    }).catch(function(error){
+        res.status(400).send(error);
+    });
+})
+
+// Enpoint for creating a bin
+app.post("/api/bins", function(req, res){
+    const bin = new Bin(req.body);
+
+    bin.save().then(function(){
+        res.status(201).send(bin);
+    }).catch(function(error){
+        res.status(400).send(error);
+    });
+
+});
+
+// Enpoint for reading all the bins
+app.get("/api/bins", function(req,res){
+    Bin.find({}).then(function(bins){
+        res.send(bins);
+    }).catch(function(error){
+        res.status(500).send(error);
+    })
+})
+
+// Enpoint for reading a specific bin
+app.get("/api/bins/:id", function(req,res){
+    Bin.findById(req.params.id).then(function(bin){
+        if(!bin){
+            return res.status(404).send({error: "Bin not found"});
+        } 
+        res.send(bin);
+        
+    }).catch(function(error){
+        res.status(400).send(error);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
