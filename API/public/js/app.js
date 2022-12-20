ab6a8ba87fc434cb16a741ed92621c9560c862ba
Ravindu Mihiranga <e18224@eng.pdn.ac.lk>
2022-12-21 01:18:24 +0530

    all done

--- API/public/js/app.js
+++ API/public/js/app.js
@@ -1,3 +1,28 @@
+const getBinData = async function(url){
+    var response =  await fetch(url);
+    var binData =  await response.json();
+
+    // var day = binData.day;
+    // var quarter = binData.quarter;
+    // var temperature = binData.temperature;
+    // var humidity = binData.humidity;
+    // var methaneOutput = binData.methaneOutput;
+    // var compostStatus = binData.compostStatus;
+
+    // await new Promise(resolve => setTimeout(resolve, 1000));
+
+    return data = {
+        "day"  : binData.day,
+        "quarter" : binData.quarter,
+        "temperature" : binData.temperature,
+        "humidity" : binData.humidity,
+        "methaneOutput" : binData.methaneOutput,
+        "compostStatus" : binData.compostStatus
+    }
+}
+
+
+
 const getBins = async function(){
     const url = "/api/bins";
 
@@ -9,11 +34,16 @@ const getBins = async function(){
             return document.querySelector("#card-wrapper").innerHTML = "<p>No Bins found!</p>";
         }
 
+        // var binsData = [];
         var binsHTML = "";
-        bins.forEach((bin) => {
-            binsHTML += generateBinCard(bin);
+        bins.forEach(  (bin) => {
+            // var binNum = bin.binNumber;
+            // var url = "/api/bindata/" + binNum;
+
+            binsHTML += generateBinCard(bin); 
         });
 
+        
         document.querySelector("#card-wrapper").innerHTML = binsHTML;
         
 
@@ -24,27 +54,47 @@ const getBins = async function(){
 }
 
 const createBin = async function(){
-    const url = "/api/bins";
+    const url1 = "/api/bins";
+    const url2 = "/api/bindata";
 
     const bin ={
         binNumber : document.querySelector("#binNumber").value,
         binLocation : document.querySelector("#binLocation").value,
         compostStatus : document.querySelector("#compostStatus").value
     }
+    
+    const data ={
+        binNumber :  document.querySelector("#binNumber").value,
+        compostStatus : document.querySelector("#compostStatus").value
+    }
 
     try{
-        const response = await fetch(url, {
+        const response1 = await fetch(url1, {
             method: "POST",
             headers: {
                 "Content-Type" : "application/json"
             },
             body : JSON.stringify(bin)
         })
-    
-        const newBin = await response.json();
+
+        const response2 = await fetch(url2, {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(data)
+        })
+
+        const newBin = await response1.json();
+        const newData = await response2.json();
+
     
         if(!newBin){
             return console.log("Unable to add Bin.");
+        }
+
+        if(!newData){
+            return console.log("Unable to add Data.");
         }
     
         const binCard = generateBinCard(newBin);
@@ -124,15 +174,19 @@ const generateBinCard = function(bin){
                     </tr>				  
                     <tr>
                     <td scope="row">Temperature</td>
-                    <td scope="row">91 °F</td>
+                    <td scope="row"> 91 °F</td>
                     </tr>
                     <tr>
                     <td scope="row">Humidity</td>
-                    <td scope="row">50 %</td>
+                    <td scope="row"> 50  %</td>
                     </tr>
                     <tr>
                     <td scope="row">Methane</td>
-                    <td scope="row">18 ppm</td>
+                    <td scope="row"> 18 ppm</td>
+                    </tr>
+                    <tr>
+                    <td scope="row">Compost Status</td>
+                    <td scope="row"> No </td>
                     </tr>
                 </tbody>
                 </table>			
@@ -146,3 +200,4 @@ const generateBinCard = function(bin){
     </div>
     `
 }
+
