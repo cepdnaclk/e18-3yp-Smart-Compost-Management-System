c75220146688075fa6e891d1420e6ed01e53d35c
Ravindu Mihiranga <e18224@eng.pdn.ac.lk>
2023-01-11 15:01:42 +0530

    added the express-session npm

--- API/src/index.js
+++ API/src/index.js
@@ -1,14 +1,18 @@
 const path = require("path");
 const express = require("express"); //  to manage servers and routes
 const hbs = require("hbs");
-
 require("./db/mongoose.js");
+const session = require("express-session");
 
 const BinDataRouter = require("./routers/BinData-router.js");
 const BinRouter = require("./routers/Bin-router.js");
+const UserRouter = require("./routers/User-router.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Setup the session
+app.use(session({secret: "something", saveUninitialized: true, resave: true}));
 
 app.set("view engine", "hbs");
 
@@ -19,6 +23,7 @@ app.use(express.json());
 
 app.use(BinDataRouter);
 app.use(BinRouter);
+app.use(UserRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
