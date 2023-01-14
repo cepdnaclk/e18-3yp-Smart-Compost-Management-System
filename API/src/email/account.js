3d59f1950ccd754235186f8f2ca35003f873b60e
Ravindu Mihiranga <e18224@eng.pdn.ac.lk>
2023-01-14 23:02:41 +0530

    account.js added

---
+++ API/src/email/account.js
@@ -0,0 +1,24 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey("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");
+
+const sendConfirmMail = async (user) => {
+    const msg = {
+        to: user.email, // Change to your recipient
+        from: 'smartcompostmanagement@gmail.com', // Change to your verified sender
+        subject: 'Confirm your account',
+        html: `
+            <p style="color:green">Welcome! ${user.companyName}, Please confirm your account by clicking the below link</p>
+            <a href="http://localhost:3000/api/users/confirm_account?userId=${user._id}&secret=${user.secret}">Confirm</a>
+        `,
+    };
+    
+    sgMail.send(msg).then((result) => {
+        console.log("Email send successfully");
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+module.exports = {
+    sendConfirmMail: sendConfirmMail
+}
