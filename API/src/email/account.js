d77f6ada0724e37862572bc6a3720be1e61f357e
Ravindu Mihiranga <e18224@eng.pdn.ac.lk>
2023-01-17 11:28:19 +0530

    dev.env added

--- API/src/email/account.js
+++ API/src/email/account.js
@@ -1,5 +1,5 @@
 const sgMail = require('@sendgrid/mail');
-
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendConfirmMail = async (user) => {
     const msg = {
@@ -8,7 +8,7 @@ const sendConfirmMail = async (user) => {
         subject: 'Confirm your account',
         html: `
             <p style="color:green">Welcome! ${user.companyName}, Please confirm your account by clicking the below link</p>
-            <a href="http://localhost:3000/api/users/confirm_account?userId=${user._id}&secret=${user.secret}">Confirm</a>
+            <a href="${process.env.DOMAIN_URL}/api/users/confirm_account?userId=${user._id}&secret=${user.secret}">Confirm</a>
         `,
     };
     
--- API/src/index.js
+++ API/src/index.js
@@ -11,7 +11,7 @@ const BinRouter = require("./routers/Bin-router.js");
 const UserRouter = require("./routers/User-router.js");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 
 // Setup the session
